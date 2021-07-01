a76b47277aa4c2bc8d423133c38a9b622c6a7bdc
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2021-07-01 13:22:25 +0300

    Add ring-ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import '@jetbrains/ring-ui/dist/style.css';
+import Alert from '@jetbrains/ring-ui/dist/alert/alert';
+import Header from '@jetbrains/ring-ui/dist/header/header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header>
+
+      </Header>
+
+        <Alert type={Alert.Type.SUCCESS}>
+          Hello!
+        </Alert>
     </div>
   );
 }
